a025f994743ebf6875fda52635fefbd5b56b3608
zhuanghua <tonyzhuanghua@gmail.com>
2018-01-05 15:09:00 +0000

    Initial commit

---
+++ src/main/java/com/yoti/entity/Dirt.java
@@ -0,0 +1,9 @@
+package com.yoti.entity;
+
+/**
+ * Created by zhuanghua on 2018/1/5.
+ */
+public class Dirt {
+
+
+}
---
+++ src/main/java/com/yoti/entity/Hoover.java
@@ -0,0 +1,7 @@
+package com.yoti.entity;
+
+/**
+ * Created by zhuanghua on 2018/1/5.
+ */
+public class Hoover {
+}
